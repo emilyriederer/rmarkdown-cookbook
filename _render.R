7ec756a773c6f423850f89638cffd213b2c86ade
Emily Riederer <emilyriederer@gmail.com>
2020-12-22 21:23:23 -0600

    temporarily disable redirect

--- _render.R
+++ _render.R
@@ -12,24 +12,24 @@ for (fmt in formats) {
 }
 unlink('rmarkdown-cookbook.log')
 
-r = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
-if (travis) for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
-  x = readLines(f)
-  if (length(i <- grep('^\\s*<body>\\s*$', x)) == 0) next
-  # patch HTML files in gh-pages if built on Travis, to redirect to bookdown.org
-  x[i[1]] = r
-  writeLines(x, f)
-}
+# r = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
+# if (travis) for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
+#   x = readLines(f)
+#   if (length(i <- grep('^\\s*<body>\\s*$', x)) == 0) next
+#   # patch HTML files in gh-pages if built on Travis, to redirect to bookdown.org
+#   x[i[1]] = r
+#   writeLines(x, f)
+# }
 
-redirect = function(from, to) {
-  to = paste0('https://bookdown.org/yihui/rmarkdown-cookbook/', to)
-  writeLines(sprintf(
-    '<html><head><meta http-equiv="refresh" content="0; URL=\'%s\'" /></head><body><script>window.location = "%s";</script></html>', to, to
-  ), paste0('_book/', from))
-}
-
-redirect('r-markdown-components.html', 'rmarkdown-process.html')
-redirect('acknowledgements.html', 'acknowledgments.html')
+# redirect = function(from, to) {
+#   to = paste0('https://bookdown.org/yihui/rmarkdown-cookbook/', to)
+#   writeLines(sprintf(
+#     '<html><head><meta http-equiv="refresh" content="0; URL=\'%s\'" /></head><body><script>window.location = "%s";</script></html>', to, to
+#   ), paste0('_book/', from))
+# }
+#
+# redirect('r-markdown-components.html', 'rmarkdown-process.html')
+# redirect('acknowledgements.html', 'acknowledgments.html')
 
 if (length(formats) > 1 && Sys.getenv('USER') == 'yihui') {
   bookdown::publish_book(account = 'yihui', server = 'bookdown.org')
